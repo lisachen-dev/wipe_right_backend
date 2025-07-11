895a204e3eccf2b29d3d0ff4fec0a66bcc73ecdd
Lisa <lisa.hl.chen@gmail.com>
2025-07-11 15:36:06 -0700

    feat: add env variable loading and DB connection setup

---
+++ db.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+from sqlmodel import SQLModel, create_engine
+import os
+
+load_dotenv() # takes environment variables
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL, echo=True) # engine object allows us to talk to db, run queries, create tables, etc
