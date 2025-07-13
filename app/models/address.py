63dbe99baf12b9e28c9f4435077a4ad4eaebeb60
Lisa <lisa.hl.chen@gmail.com>
2025-07-13 00:47:04 -0700

    fix: add customer_id to addresses table

--- app/models/address.py
+++ app/models/address.py
@@ -1,9 +1,12 @@
+import uuid
 from mixins import TimestampMixin
 from typing import Optional
+from sqlmodel import Field
 
 class Address (TimestampMixin, table=True):
     __tablename__ = "addresses"
 
+    customer_id: uuid.UUID = Field(foreign_key="customers.id")
     street_address_1: str
     street_address_2: Optional[str] = None
     city: str
