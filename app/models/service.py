e0ed8bb5ccdcb68fac4bff7ce560de904fa0c492
luisarevalo21 <arevaloluis89@yahoo.com>
2025-07-17 16:39:49 -0700

    added backend route for services/categories based on the num and returns empty [] if not found

--- app/models/service.py
+++ app/models/service.py
@@ -2,12 +2,23 @@ from typing import Optional
 from uuid import UUID, uuid4
 from datetime import datetime
 from sqlmodel import SQLModel, Field, Column, DateTime, text
+from enum import Enum
+
+class ServiceEnum( Enum): 
+    HOUSE_CLEANING = "House Cleaning"
+    LAWN_AND_GARDEN = "Lawn & Garden"
+    HANDYMAN_AND_REPAIRS = "Handyman & Repairs"
+    EXTERIOR_CLEANING = "Exterior Cleaning"
+    SPECIALIZED_CLEANING = "Specialized Cleaning"
+    ASSEMBLY_AND_INSTALLATION = "Assembly & Installation"
+
 
 class ServiceBase(SQLModel):
     service_title: str
     service_description: Optional[str] = None
     pricing: float
     duration: int  # in minutes
+    category: str
 
 class Service(ServiceBase, table=True):
     __tablename__ = "services"
--- app/routers/service.py
+++ app/routers/service.py
@@ -2,15 +2,13 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 
-from app.models.service import Service, ServiceCreate, ServiceUpdate
+from app.models.service import Service, ServiceCreate, ServiceUpdate, ServiceEnum
 from app.db.session import get_session
 from app.utils.crud_helpers import get_all, get_one, create_one, update_one, delete_one
 # TODO from app.utils.auth import get_current_user  # optional for auth-locking
 
 router = APIRouter(
-    prefix="/services",
-    tags=["services"],
-    responses={404: {"description": "Not found"}}
+    prefix="/services", tags=["services"], responses={404: {"description": "Not found"}}
 )
 
 # GET all services
@@ -22,6 +20,25 @@ async def read_services(session: Session = Depends(get_session)):
 @router.get("/{service_id}", response_model=Service)
 async def read_service(service_id: UUID, session: Session = Depends(get_session)):
     return get_one(session, Service, service_id)
+
+# GET service by category 
+@router.get("/category/{category_name}", response_model=list[Service])
+async def read_service_category(category_name: str, session: Session = Depends(get_session)):
+    valid_categories = [e.value for e in ServiceEnum]
+    
+    if category_name not in valid_categories:
+        # return []
+        raise HTTPException(status_code=400, detail=f"invalid category, valid categories are: {valid_categories}", )
+    try:
+        statement = select(Service).where(Service.category == category_name)
+        result = session.exec(statement)
+        return result.all() 
+    
+    except: 
+        print(f'except called', flush=True)
+        raise HTTPException(status_code=400, detail=f"{Service.__name__} not found")
+
+
 
 # CREATE a service
 @router.post("/", response_model=Service)
